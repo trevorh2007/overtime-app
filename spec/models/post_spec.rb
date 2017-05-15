edea06c7221d9bd93e7d853f06b89ed5611d2104
Trevor <trevorhoward2@hotmail.com>
2017-05-15 15:04:38 -0600

    Implemented edit and update functionality

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,19 +3,17 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   describe "Creation" do
     before do
-      user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
-      login_as(user, :scope => :user)
-      @post = Post.create(date: Date.today, rationale: "Anything", user_id: user.id)
+      @post = FactoryGirl.create(:post)
     end
 
-  	it 'can be created' do	
-  		expect(@post).to be_valid
-  	end
+    it 'can be created' do  
+      expect(@post).to be_valid
+    end
 
-  	it 'cannot be created without a date and rationale' do
-  		@post.date = nil
-  		@post.rationale = nil
-  		expect(@post).to_not be_valid
-  	end
+    it 'cannot be created without a date and rationale' do
+      @post.date = nil
+      @post.rationale = nil
+      expect(@post).to_not be_valid
+    end
   end
 end
