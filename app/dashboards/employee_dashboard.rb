5b529c6535197b19e15093e9ffee51beceb11cca
Trevor <trevorhoward2@hotmail.com>
2017-05-17 14:59:24 -0600

    Integrated permissions to hide status form elements from non admins

--- app/dashboards/employee_dashboard.rb
+++ app/dashboards/employee_dashboard.rb
@@ -1,6 +1,6 @@
 require "administrate/base_dashboard"
 
-class UserDashboard < Administrate::BaseDashboard
+class EmployeeDashboard < Administrate::BaseDashboard
   ATTRIBUTE_TYPES = {
     posts: Field::HasMany.with_options(searchable: false),
     id: Field::Number.with_options(searchable: false),
@@ -16,17 +16,23 @@ class UserDashboard < Administrate::BaseDashboard
     created_at: Field::DateTime.with_options(searchable: false),
     updated_at: Field::DateTime.with_options(searchable: false),
     type: Field::String.with_options(searchable: false),
+    phone: Field::String.with_options(searchable: false),
+    ssn: Field::Number.with_options(searchable: true),
+    company: Field::String.with_options(searchable: false),
   }.freeze
 
   COLLECTION_ATTRIBUTES = [
     :posts,
     :email,
-    :type
+    :ssn
   ].freeze
 
   SHOW_PAGE_ATTRIBUTES = [
     :posts,
+    :ssn,
+    :company,
     :email,
+    :phone,
     :sign_in_count,
     :current_sign_in_at,
     :last_sign_in_at,
@@ -40,9 +46,13 @@ class UserDashboard < Administrate::BaseDashboard
   ].freeze
 
   FORM_ATTRIBUTES = [
+    :ssn,
     :email,
     :password,
     :first_name,
     :last_name,
+    :company,
+    :type,
+    :phone,
   ].freeze
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,12 @@
 
 puts "1 User created"
 
+AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Admin", last_name: "Name")
+
+puts "1 Admin User created"
+
 100.times do |post|
 	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
 
-puts "100 Posts created"
+puts "100 Posts have been created"
