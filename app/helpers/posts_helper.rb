f9bccfca1f413b5b4e166d7c1f335db7b3a11d08
Trevor <trevorhoward2@hotmail.com>
2017-05-18 16:59:34 -0600

    Implemented labels with status values on index page

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,16 @@
 module PostsHelper
+
+	def status_label status
+		status_span_generator status
+	end
+
+	private
+
+	def status_span_generator status
+		case status
+			when 'submitted' then content_tag(:span, status.capitalize, class: 'label label-primary')
+			when 'approved' then content_tag(:span, status.capitalize, class: 'label label-success')
+			when 'rejected' then content_tag(:span, status.capitalize, class: 'label label-danger')
+		end
+	end
 end
