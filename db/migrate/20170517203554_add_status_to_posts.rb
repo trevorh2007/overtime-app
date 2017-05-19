9865effabb8ca031b36818bd85522957dd8a02e8
Trevor <trevorhoward2@hotmail.com>
2017-05-19 14:55:42 -0600

    Added hours requested to post model

--- db/migrate/20170517203554_add_status_to_posts.rb
+++ db/migrate/20170517203554_add_status_to_posts.rb
@@ -1,5 +1,5 @@
 class AddStatusToPosts < ActiveRecord::Migration[5.0]
   def change
-    add_column :posts, :status, :integer
+    add_column :posts, :status, :integer, default: 0
   end
 end
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,12 +2,14 @@ FactoryGirl.define do
   factory :post do
     date Date.today
     rationale "Some rationale"
+    overtime_request 4.2
     user
   end
 
   factory :second_post, class: "Post" do
     date Date.yesterday
     rationale "Some more content"
+    overtime_request 4.12
     user
   end
 
