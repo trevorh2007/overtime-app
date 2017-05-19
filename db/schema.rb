44da770e3ca7af58012cbbb052894a024a504e9a
Trevor <trevorhoward2@hotmail.com>
2017-05-19 15:09:52 -0600

    Updated show page with styles

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20170519195554) do
     t.datetime "created_at",                       null: false
     t.datetime "updated_at",                       null: false
     t.integer  "user_id"
-    t.integer  "status"
+    t.integer  "status",           default: 0
     t.decimal  "overtime_request", default: "0.0", null: false
     t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
