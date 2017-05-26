1004d0c33a03cea0cccd5ecadcc0b003de205ea2
Trevor <trevorhoward2@hotmail.com>
2017-05-25 22:09:04 -0600

    Implemented functional buttons for employee homepage confirmations

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -12,4 +12,17 @@ describe 'Homepage' do
 
 		expect(post.reload.status).to eq('approved')
 	end
+
+	it 'allows employee to approve audit log status from homepage' do
+		audit_log = FactoryGirl.create(:audit_log)
+		user = FactoryGirl.create(:user)
+		login_as(user, :scope => :user)
+		audit_log.update(user_id: user.id)
+
+		visit root_path
+
+		click_on("confirm_#{audit_log.id}")
+
+		expect(audit_log.reload.status).to eq('confirmed')
+	end
 end
