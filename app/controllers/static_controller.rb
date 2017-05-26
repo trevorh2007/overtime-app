d8c0125255f905482a0f1895490bc8690cfe0a1c
Trevor <trevorhoward2@hotmail.com>
2017-05-26 00:10:30 -0600

    Updated employee homepage to include recent overtime request and view their status

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,7 @@
 class StaticController < ApplicationController
   def homepage
+  	@pending_approvals = Post.submitted
   	if admin_types.include?(current_user.type)
-  		@pending_approvals = Post.submitted
   		@recent_audit_items = AuditLog.last(10)
   	else
   		@pending_audit_confirmations = current_user.audit_logs.pending
