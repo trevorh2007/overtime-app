6ec41986ce19dc840a4840ee0add0db6937b30a0
Trevor <trevorhoward2@hotmail.com>
2017-05-26 17:46:35 -0600

    Updated Confirmation log to show only pending logs

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,7 +2,7 @@ class StaticController < ApplicationController
   def homepage
   	@pending_approvals = Post.submitted
   	if admin_types.include?(current_user.type)
-  		@recent_audit_items = AuditLog.last(10)
+  		@recent_audit_items = AuditLog.pending.last(10)
   	else
   		@pending_audit_confirmations = current_user.audit_logs.pending
   	end
