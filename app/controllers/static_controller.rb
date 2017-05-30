9e6aa7afaac573571bddaf7a9abbb48e9e25495c
Trevor <trevorhoward2@hotmail.com>
2017-05-29 23:29:22 -0600

    Refactored button functionality for employee dashboard

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -5,7 +5,7 @@ class StaticController < ApplicationController
   		@recent_audit_items = AuditLog.pending.last(10)
   		@recent_confirmed_audit_items = AuditLog.order('updated_at DESC').confirmed.last(10)
   	else
-  		@pending_audit_confirmations = current_user.audit_logs.pending
+  		@pending_audit_confirmations = current_user.audit_logs.pending.by_start_date
   	end
   end
 end
